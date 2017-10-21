36fa00194454b756fdf68c6bfba71faf1776a549
nophyon <juantago.games@gmail.com>
2017-10-21 20:44:18 +0200

    JJJ

--- Dwec/U2/Prac1/Ej7.js
+++ Dwec/U2/Prac1/Ej7.js
@@ -1,5 +1,4 @@
 "use strict"
-
 var frase = "Esto es un string";
 console.log(frase);
 console.log("a) La longitud del string es: "+ frase.length);
@@ -8,9 +7,6 @@ console.log("c) El índice en el que está la última t es: "+frase.lastIndexOf(
 console.log("d) La transformación a minúsculas del string es: "+frase.toLowerCase());
 console.log("e) La transformación de mayúsculas del string es: "+frase.toUpperCase());
 console.log("f) Las palabras separadas por comas que componen la frase son: "+frase.split(" "));
-
-console.log("g) las posiciones de la 11 a la 18 de frase son: "+frase.charAt(11)+" y "+frase.charAt(18));
-
 console.log("g) las posiciones de la 11 a la 18 de frase son: "+frase.charAt(11)+frase.charAt(12)+frase.charAt(13)+frase.charAt(14)+
   frase.charAt(15)+frase.charAt(16)+frase.charAt(17)+frase.charAt(18));
 //console.log(frase.slice(11,18));
